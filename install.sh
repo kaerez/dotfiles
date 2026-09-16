d55fdddc40359f5d24b27bef0add4ff8bf4f4751
kaerez <kaerez@users.noreply.github.com>
2026-09-16 13:04:30 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+mkdir -p ~/.config/Code\ -\ OSS/User/snippets
+ln -sf ~/dotfiles/settings.json ~/.config/Code\ -\ OSS/User/settings.json
+ln -sf ~/dotfiles/snippets/* ~/.config/Code\ -\ OSS/User/snippets/
